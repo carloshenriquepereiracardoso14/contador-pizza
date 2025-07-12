4f81fe212a8211f874e1a3ff5dbf1da69c470b34
carloshenriquepereiracardoso14 <carloscardosoesse@gmail.com>
2025-07-12 20:26:21 -0300

    Update js.js

--- src/js/js.js
+++ src/js/js.js
@@ -115,3 +115,9 @@ function ajustarBarra() {
         barra.style.width = tamanhoBarra;
     }
 }
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(() => console.log("Service Worker registrado com sucesso!"))
+      .catch((error) => console.log("Erro ao registrar o Service Worker:", error));
+}
