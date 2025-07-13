5e265b352db7142fbb07d2c69931871e45a2f695
carloshenriquepereiracardoso14 <carloscardosoesse@gmail.com>
2025-07-13 15:38:19 -0300

    att web app

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'contador-pizza-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './main.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request)
+      .then(resp => resp || fetch(evt.request))
+  );
+});
